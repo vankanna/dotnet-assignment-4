173c7f98cc01ec084d25cb52f84428c8a4745eb4
vankanna <passvankanna@gmail.com>
2021-09-27 23:04:02 -0500

    added product page

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -10,6 +10,6 @@ namespace Northwind
         public ProductController(NorthwindContext db) => _northwindContext = db;
 
         public IActionResult Category() => View(_northwindContext.Categories.OrderBy(category => category.CategoryName));
-       
+        public IActionResult Index(int id) => View(_northwindContext.Products.Where(product => product.CategoryId == id && product.Discontinued == false).OrderBy(product => product.ProductName));
     }
 }
--- Models/Product.cs
+++ Models/Product.cs
@@ -1,6 +1,6 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
-
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Northwind.Models
 {
@@ -9,6 +9,7 @@ namespace Northwind.Models
         public int ProductId { get; set; }
         public string ProductName { get; set; }
         public string QuantityPerUnit { get; set; }
+        [Column(TypeName = "decimal(18,4)")]
         public decimal UnitPrice { get; set; }
         public short UnitsInStock { get; set; }
         public short UnitsOnOrder { get; set; }
